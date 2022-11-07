621c050d7ca0c9b059fd38aca3421f58564af6d5
juanmarcos <acevedojuan17@gmail.com>
2022-11-07 13:32:08 -0500

    JSX exercises

--- books/road-to-react/01_fundamentals/02_components_jsx/src/App.js
+++ books/road-to-react/01_fundamentals/02_components_jsx/src/App.js
@@ -1,13 +1,41 @@
-const titleOutsideFunction = 'This is a variable outside the function'
+const titleOutsideFunction = 'this is a variable outside the function'
+
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+}
+
+const arrayDePrueba = [
+  1,
+  2,
+  3,
+  4
+]
+
+const willkommen = (title) => title;
 
 function App() {
   //You can do something in between
-  const titleInFunction = 'This is variable inside the function';
+  const titleInFunction = 'this is variable inside the function';
   return (
     <div>
       <h1>Hello, World!</h1>
-      <h2>{titleOutsideFunction}</h2>
-      <h2>{titleInFunction}</h2>
+      <h2>{welcome.greeting}, {welcome.title}!</h2>
+      <h2>Now, in German: {willkommen('Hey, willkommen!')}</h2>
+      <h2>Hello, {titleOutsideFunction}</h2>
+      <h2>Hello, {titleInFunction}</h2>
+      <h3>{arrayDePrueba[0]}</h3>
+      <h3>{arrayDePrueba.map((item) => {
+        return (
+        <div>{item}</div>
+        );
+      })}</h3>
+
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+    </div>
+
     </div>
   );
 }
