30611f59fb14d5d794cd06c4f377f360af665147
juanmarcos <acevedojuan17@gmail.com>
2023-01-21 17:10:24 -0500

    Corrigiendo constructor y definiendo metodos de clase ProductManager

--- courses/coderhouse/01_desarrollo-full-stack/04_backend/00_entregables/clase-03_clases-ecmascript/index.js
+++ courses/coderhouse/01_desarrollo-full-stack/04_backend/00_entregables/clase-03_clases-ecmascript/index.js
@@ -1,25 +1,57 @@
 // Creando clase ProductManager.
 class ProductManager {
-  constructor(title, description, price, thumbnail, code, stock) {
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.thumbnail = thumbnail;
-    this.code = code;
-    this.stock = stock;
+  constructor() {
+    this.product = [];
   };
-  // Agrega un producto a arreglo de productos.
-  // Valida que no existan productos repetidos y que todos los campos sean obligatorios.
-  addProduct() {
-    return 'testing addProduct method...';
+  // Agrega un producto a arreglo de productos. Valida que no existan productos repetidos y que todos los campos sean obligatorios.
+  addProduct(title, description, price, thumbnail, code, stock) {
+    if (title === "" || description === "" || price === "" || thumbnail === "" || code === "" || stock === "") {
+      console.log("Not enough information to create a new product");
+    } else {
+      if (!this.product.length == 0) {
+        const codeToCompare = this.product.find(element => element.code === code);
+        if (!codeToCompare) {
+          this.product.push({id: this.product.length + 1, title, description, price, thumbnail, code, stock});
+        } else {
+          console.log("There is already a product with that code");
+        }
+      } else {
+        this.product.push({id: this.product.length + 1, title, description, price, thumbnail, code, stock});
+      };
+    };
   };
   // Devuelve arreglo con prudctos agregados.
   getProducts() {
-    return 'testing getProducts method...';
+    return this.product;
   };
   // Busca arreglo a través del id suministrado por el usuario.
   // Devuelve error en caso de que el id no exista.
-  getProductById() {
-    return 'testing getProductById method...';
+  getProductById(id) {
+    if (this.product.length === 0) {
+      console.log("List is empty. Can't get any product by id");
+    } else {
+      console.log("testin getProdcutById method...");
+      const idToFind = this.product.find(element => element.id === id);
+      if (idToFind) {
+        console.log(this.product[id - 1]);
+        return this.product[id - 1];
+      } else {
+        console.log(`There is no product with id ${id}`);
+      }
+    }
+
   };
 };
+
+
+// Testing
+// const productsList = new ProductManager();
+// productsList.getProductById();
+// productsList.addProduct(1,"hola",3,4,5,6);
+// productsList.addProduct(1,"hola",3,4,5,6);
+// productsList.addProduct(1,"hola",3,4,9,6);
+// productsList.addProduct("a", "b", "c", "d", "e", "f");
+// productsList.addProduct("a", "b", "c", "d", "juan", "f");
+// const productListA = productsList.getProducts();
+// console.log(productListA);
+// const getElement = productsList.getProductById(3);
