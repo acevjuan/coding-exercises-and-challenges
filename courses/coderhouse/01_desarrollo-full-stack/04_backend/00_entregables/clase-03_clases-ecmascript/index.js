5357400dc124d8bc1c62b7c75a3f495907fbc2d2
juanmarcos <acevedojuan17@gmail.com>
2023-01-22 10:26:32 -0500

    Refactorizando código + testing

--- courses/coderhouse/01_desarrollo-full-stack/04_backend/00_entregables/clase-03_clases-ecmascript/index.js
+++ courses/coderhouse/01_desarrollo-full-stack/04_backend/00_entregables/clase-03_clases-ecmascript/index.js
@@ -1,57 +1,82 @@
 // Creando clase ProductManager.
 class ProductManager {
+  // El constructor genera un arreglo vacío.
   constructor() {
     this.product = [];
   };
-  // Agrega un producto a arreglo de productos. Valida que no existan productos repetidos y que todos los campos sean obligatorios.
+  
+  // Agrega un producto a arreglo de productos. Valida que no existan productos repetidos y que todos los campos sean obligatorios. Igualmente, genera un id autoincrementable para cada nuevo producto agregado.
   addProduct(title, description, price, thumbnail, code, stock) {
+    // Validación de que todos los campos sean diligenciados.
     if (title === "" || description === "" || price === "" || thumbnail === "" || code === "" || stock === "") {
-      console.log("Not enough information to create a new product");
+      console.log("Not enough information to create a new product.");
     } else {
+      // Validación de que no existan propiedades "code" iguales.
+      // Verificación de que el arreglo ya cuente con productos agregados y que se realice la verificación de "code".
       if (!this.product.length == 0) {
         const codeToCompare = this.product.find(element => element.code === code);
         if (!codeToCompare) {
+          // Crea y agrega nuevo producto al arreglo.
           this.product.push({id: this.product.length + 1, title, description, price, thumbnail, code, stock});
         } else {
-          console.log("There is already a product with that code");
-        }
+          // En caso de que ya exista el mismo "code", notificará al usuario del error y detendrá la creación de nuevo producto.
+          console.log("Error. There is already a product with the same code.");
+        };
       } else {
         this.product.push({id: this.product.length + 1, title, description, price, thumbnail, code, stock});
       };
     };
   };
-  // Devuelve arreglo con prudctos agregados.
+  
+  // Devuelve arreglo con productos agregados.
   getProducts() {
     return this.product;
   };
+  
   // Busca arreglo a través del id suministrado por el usuario.
   // Devuelve error en caso de que el id no exista.
   getProductById(id) {
+    // Validación de que existan productos agregados. En caso que no, notificará al usuario.
     if (this.product.length === 0) {
       console.log("List is empty. Can't get any product by id");
     } else {
-      console.log("testin getProdcutById method...");
+      // Procede a buscar el id suministrado por el usuario.
       const idToFind = this.product.find(element => element.id === id);
+      // Si el id suministrado por el usuario existe, se devolverá un objeto con el producto deseado.
       if (idToFind) {
         console.log(this.product[id - 1]);
         return this.product[id - 1];
       } else {
-        console.log(`There is no product with id ${id}`);
-      }
-    }
-
+        // Si no existe el arreglo, se notificará al usuario por medio de un error.
+        console.log(`Not found. There is no product with id ${id}`);
+      };
+    };
   };
 };
 
-
 // Testing
-// const productsList = new ProductManager();
-// productsList.getProductById();
-// productsList.addProduct(1,"hola",3,4,5,6);
-// productsList.addProduct(1,"hola",3,4,5,6);
-// productsList.addProduct(1,"hola",3,4,9,6);
-// productsList.addProduct("a", "b", "c", "d", "e", "f");
-// productsList.addProduct("a", "b", "c", "d", "juan", "f");
-// const productListA = productsList.getProducts();
-// console.log(productListA);
-// const getElement = productsList.getProductById(3);
+// Creando instancia de la clase ProductoManager
+const testingProduct = new ProductManager();
+
+// Probando método getProducts
+let productList = testingProduct.getProducts();
+console.log("Devuelve un arreglo vacío.");
+console.log(productList);
+
+// Probando método addProducto
+testingProduct.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+productList = testingProduct.getProducts();
+console.log("Devuelve arreglo con producto recién creado con id = 1.");
+console.log(productList);
+console.log("Se agrega un producto exactamente igual para probar que no el método addProducto no lo permite por tener el mismo code.");
+testingProduct.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+console.log("Se imprime nuevamente productList para demostrar que el arreglo no fue alterado.");
+console.log(productList);
+
+// Probando método getProductById
+let productById;
+// En caso de no existir el id
+productById = testingProduct.getProductById(2);
+console.log(productById);
+// En caso de existir el id
+productById = testingProduct.getProductById(1);
